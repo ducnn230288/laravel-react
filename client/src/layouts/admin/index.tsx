22c27edddfa0745921b4ea68c353f1da241d8ad9
ducnn230288 <ducnn230288@gmail.com>
2024-06-02 10:05:48 +0700

    fix bug

--- client/src/layouts/admin/index.tsx
+++ client/src/layouts/admin/index.tsx
@@ -1,25 +1,19 @@
-import React, { PropsWithChildren, useEffect } from 'react';
-import { Dropdown } from 'antd';
+import React, { PropsWithChildren } from 'react';
+import { Dropdown, Menu } from 'antd';
 import { useTranslation } from 'react-i18next';
-import { useNavigate } from 'react-router';
+import { useLocation, useNavigate } from 'react-router';
 import classNames from 'classnames';
 
 import { Avatar } from '@/library/avatar';
 import { SGlobal } from '@/services';
 import { Key, Out, User, Logo, DayNight } from '@/assets/svg';
 import { routerLinks, lang, APP_NAME } from '@/utils';
-
-import Menu from './menu';
+import menus, { findMenu } from './menus';
+import { createSearchParams } from 'react-router-dom';
 
 const Layout = ({ children }: PropsWithChildren) => {
   const { t } = useTranslation('locale', { keyPrefix: 'layouts' });
   const sGlobal = SGlobal();
-
-  useEffect(() => {
-    window.scrollTo({ top: 0, behavior: 'smooth' });
-
-    return () => {};
-  }, []);
 
   return (
     <main className="flex">
@@ -48,12 +42,11 @@ const Layout = ({ children }: PropsWithChildren) => {
             {APP_NAME}
           </h1>
         </a>
-        <Menu permission={sGlobal.user?.role?.permissions} />
+        <CMenu />
       </aside>
 
-      {/* <div className={'w-full h-full fixed bg-black opacity-30 z-20'} /> */}
       <section className={'grow bg-base-300/50 px-2 sm:px-0'}>
-        <Header />
+        <CHeader />
         <div className={'h-[calc(100vh-6rem)] overflow-auto'}>{children}</div>
 
         <footer className="w-full pt-1.5 text-center">{t('Footer', { year: new Date().getFullYear() })}</footer>
@@ -61,10 +54,95 @@ const Layout = ({ children }: PropsWithChildren) => {
     </main>
   );
 };
-const Header = () => {
+
+const CMenu = () => {
+  const sGlobal = SGlobal();
+  const location = useLocation();
+  const navigate = useNavigate();
+  const listMenu = menus({ lang: sGlobal.language, permissions: sGlobal.user?.role?.permissions });
+
+  return (
+    <Menu
+      className="main-menu"
+      defaultSelectedKeys={[location.pathname]}
+      defaultOpenKeys={['/' + location.pathname.substring(1).split('/').slice(0, 2).join('/')]}
+      mode="inline"
+      inlineCollapsed={sGlobal.isCollapseMenu}
+      items={listMenu as any}
+      onSelect={({ key }) => {
+        const menu = findMenu(listMenu, key);
+        if (location.pathname !== key && menu) {
+          navigate({
+            pathname: menu.key,
+            search: `?${createSearchParams(menu.queryparams)}`,
+          });
+        }
+      }}
+    />
+  );
+};
+const CHeader = () => {
   const { t } = useTranslation('locale', { keyPrefix: 'layouts' });
   const sGlobal = SGlobal();
   const navigate = useNavigate();
+
+  const listDropdown = [
+    {
+      key: '0',
+      className: 'hover:!bg-white border-b-slate-300 border-b',
+      label: (
+        <div className="flex">
+          <Avatar src={sGlobal.user?.avatar ?? ''} size={8} />
+          <div className="mr-3 block pl-2 text-left leading-none">
+            <div className="mb-0.5 text-sm font-semibold leading-snug">{sGlobal.user?.name}</div>
+            <div className="text-[10px] text-gray-500">{sGlobal.user?.email}</div>
+          </div>
+        </div>
+      ),
+    },
+    {
+      key: '1',
+      className: 'h-11',
+      label: (
+        <button
+          className="flex"
+          onClick={() => navigate(`/${lang}${routerLinks('MyProfile')}?tab=1`, { replace: true })}
+        >
+          <div className="flex items-center">
+            <User className="size-6 pr-2" />
+          </div>
+          <div>{t('My Profile')}</div>
+        </button>
+      ),
+    },
+    {
+      key: '2',
+      className: 'h-11 !border-b-slate-300 border-b !rounded-none',
+      label: (
+        <button
+          className="flex"
+          onClick={() => navigate(`/${lang}${routerLinks('MyProfile')}?tab=2`, { replace: true })}
+        >
+          <div className="flex items-center">
+            <Key className="size-6 pr-2" />
+          </div>
+          <div>{t('Change Password')}</div>
+        </button>
+      ),
+    },
+    {
+      key: '3',
+      className: 'h-11',
+      label: (
+        <button className="flex" onClick={() => navigate(`/${lang}${routerLinks('Login')}`, { replace: true })}>
+          <div className="flex items-center">
+            <Out className="size-6 pr-2" />
+          </div>
+          <div>{t('Sign out')}</div>
+        </button>
+      ),
+    },
+  ];
 
   return (
     <header className={'block border-b border-base-300 bg-base-100'}>
@@ -83,72 +161,7 @@ const Header = () => {
         </div>
 
         <div className="flex items-center">
-          <Dropdown
-            trigger={['click']}
-            menu={{
-              items: [
-                {
-                  key: '0',
-                  className: 'hover:!bg-white border-b-slate-300 border-b',
-                  label: (
-                    <div className="flex">
-                      <Avatar src={sGlobal.user?.avatar ?? ''} size={8} />
-                      <div className="mr-3 block pl-2 text-left leading-none">
-                        <div className="mb-0.5 text-sm font-semibold leading-snug">{sGlobal.user?.name}</div>
-                        <div className="text-[10px] text-gray-500">{sGlobal.user?.email}</div>
-                      </div>
-                    </div>
-                  ),
-                },
-                {
-                  key: '1',
-                  className: 'h-11',
-                  label: (
-                    <button
-                      className="flex"
-                      onClick={() => navigate(`/${lang}${routerLinks('MyProfile')}?tab=1`, { replace: true })}
-                    >
-                      <div className="flex items-center">
-                        <User className="size-6 pr-2" />
-                      </div>
-                      <div>{t('My Profile')}</div>
-                    </button>
-                  ),
-                },
-                {
-                  key: '2',
-                  className: 'h-11 !border-b-slate-300 border-b !rounded-none',
-                  label: (
-                    <button
-                      className="flex"
-                      onClick={() => navigate(`/${lang}${routerLinks('MyProfile')}?tab=2`, { replace: true })}
-                    >
-                      <div className="flex items-center">
-                        <Key className="size-6 pr-2" />
-                      </div>
-                      <div>{t('Change Password')}</div>
-                    </button>
-                  ),
-                },
-                {
-                  key: '3',
-                  className: 'h-11',
-                  label: (
-                    <button
-                      className="flex"
-                      onClick={() => navigate(`/${lang}${routerLinks('Login')}`, { replace: true })}
-                    >
-                      <div className="flex items-center">
-                        <Out className="size-6 pr-2" />
-                      </div>
-                      <div>{t('Sign out')}</div>
-                    </button>
-                  ),
-                },
-              ],
-            }}
-            placement="bottomRight"
-          >
+          <Dropdown trigger={['click']} menu={{ items: listDropdown }} placement="bottomRight">
             <div className="flex gap-3">
               <button
                 className="rounded-btn px-2 py-1 hover:bg-primary/10 hover:text-primary"
--- client/src/layouts/admin/menus.tsx
+++ client/src/layouts/admin/menus.tsx
@@ -1,55 +1,84 @@
 import React from 'react';
 import { URLSearchParamsInit } from 'react-router-dom/dist/dom';
 
-import { keyRole } from '@/utils';
+import { keyRole, language, routerLinks } from '@/utils';
 import { Calendar, Cog, User } from '@/assets/svg';
-import './index.less';
-
-const Layout: IMenu[] = [
-  {
-    icon: <Calendar className="h-6 w-5" />,
-    label: 'Dashboard',
-  },
-  {
-    icon: <User className="h-6 w-5" />,
-    label: 'User',
-    permission: keyRole.P_USER_INDEX,
-    queryParams: { roleCode: 'SUPER-ADMIN' },
-  },
-  {
-    icon: <Cog className="h-6 w-5" />,
-    label: 'Setting',
-    children: [
-      {
-        label: 'Code',
-        permission: keyRole.P_CODE_INDEX,
-        queryParams: { typeCode: 'POSITION' },
-      },
-      {
-        label: 'Content',
-        permission: keyRole.P_CONTENT_INDEX,
-        queryParams: { typeCode: 'MEMBERS' },
-      },
-      {
-        label: 'Post',
-        permission: keyRole.P_POST_INDEX,
-        queryParams: { typeCode: 'PROJECTS' },
-      },
-      {
-        label: 'Parameter',
-        permission: keyRole.P_PARAMETER_INDEX,
-        queryParams: { code: 'ADDRESS' },
-      },
-    ],
-  },
-];
-
-export default Layout;
 
 interface IMenu {
-  label: string;
+  key?: string;
+  label?: string;
   icon?: React.JSX.Element;
   permission?: keyRole;
-  queryParams?: URLSearchParamsInit;
+  queryparams?: URLSearchParamsInit;
   children?: IMenu[];
 }
+
+const Layout = ({ lang = language, permissions = [] }: { lang?: string; permissions?: string[] }): IMenu[] => {
+  const list: IMenu[] = [
+    {
+      key: `/${lang}${routerLinks('Dashboard')}`,
+      icon: <Calendar className="h-6 w-5" />,
+      label: 'Dashboard',
+    },
+    {
+      key: `/${lang}${routerLinks('User')}`,
+      icon: <User className="h-6 w-5" />,
+      label: 'User',
+      permission: keyRole.P_USER_INDEX,
+      queryparams: { roleCode: 'SUPER-ADMIN' },
+    },
+    {
+      key: `/${lang}${routerLinks('Setting')}`,
+      icon: <Cog className="h-6 w-5" />,
+      label: 'Setting',
+      children: [
+        {
+          key: `/${lang}${routerLinks('Code')}`,
+          label: 'Code',
+          permission: keyRole.P_CODE_INDEX,
+          queryparams: { typeCode: 'POSITION' },
+        },
+        {
+          key: `/${lang}${routerLinks('Content')}`,
+          label: 'Content',
+          permission: keyRole.P_CONTENT_INDEX,
+          queryparams: { typeCode: 'MEMBERS' },
+        },
+        {
+          key: `/${lang}${routerLinks('Post')}`,
+          label: 'Post',
+          permission: keyRole.P_POST_INDEX,
+          queryparams: { typeCode: 'PROJECTS' },
+        },
+        {
+          key: `/${lang}${routerLinks('Parameter')}`,
+          label: 'Parameter',
+          permission: keyRole.P_PARAMETER_INDEX,
+          queryparams: { code: 'ADDRESS' },
+        },
+      ],
+    },
+  ];
+  return list.filter((item) => {
+    return (
+      !item.permission ||
+      (!item.children && item.permission && permissions?.includes(item.permission!)) ||
+      (item.children &&
+        item.children.filter((subItem) => !subItem.permission || permissions?.includes(subItem.permission)).length > 0)
+    );
+  });
+};
+export default Layout;
+
+export const findMenu = (menus: IMenu[], key: string): IMenu | undefined => {
+  let menuCurrent: IMenu | undefined;
+  const forEachMenu = (menu: IMenu) => {
+    if (menu.key === key) {
+      menuCurrent = menu;
+    } else if (menu.children) {
+      menu.children.forEach(forEachMenu);
+    }
+  };
+  menus.forEach(forEachMenu);
+  return menuCurrent;
+};
